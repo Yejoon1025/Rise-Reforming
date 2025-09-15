8137e28b5a1b078c18b7ca87ed916f566e118f90
Yejoon1025 <yejoon1025@gmail.com>
2025-09-15 17:05:27 -0500

    Updates

--- assets/chevron-right-CSgq9cAG.js
+++ assets/chevron-right-CSgq9cAG.js
@@ -1,4 +1,4 @@
-import{c as o}from"./createLucideIcon-7kviQO5k.js";/**
+import{c as o}from"./createLucideIcon-Cz4J0MCO.js";/**
  * @license lucide-react v0.540.0 - ISC
  *
  * This source code is licensed under the ISC license.
