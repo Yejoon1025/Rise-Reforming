5db90d268439e6da45be6dd9d549fd65fa22a554
Yejoon1025 <yejoon1025@gmail.com>
2025-09-26 16:20:22 -0500

    Updates

---
+++ assets/Profiles-B22rAZlo.js
@@ -0,0 +1,21 @@
+import{c as i}from"./createLucideIcon-svdGauXJ.js";/**
+ * @license lucide-react v0.540.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=i("chevron-left",e);/**
+ * @license lucide-react v0.540.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z",key:"c2jq9f"}],["rect",{width:"4",height:"12",x:"2",y:"9",key:"mk3on5"}],["circle",{cx:"4",cy:"4",r:"2",key:"bt5ra8"}]],c=i("linkedin",n);/**
+ * @license lucide-react v0.540.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7",key:"132q7q"}],["rect",{x:"2",y:"4",width:"20",height:"16",rx:"2",key:"izxlao"}]],s=i("mail",o);/**
+ * @license lucide-react v0.540.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8",key:"1p45f6"}],["path",{d:"M21 3v5h-5",key:"1q7to0"}]],l=i("rotate-cw",a),g="/assets/TableBright-v58TEBtW.png",d=[{id:"1",name:"George Rose",title:"Co-Founder / CEO",photo:{src:"/George.jpg",alt:"George"},description:"Molecular Engineering",linkedin:"https://www.linkedin.com/in/george-rose25/",email:"george@rise-reforming.com",backImage:{src:"/UChicago.jpg",alt:"Uchicago"}},{id:"2",name:"Lucas Zubillaga",title:"Co-Founder / CTO",photo:{src:"/Lucas.png",alt:"Lucas"},description:"Molecular Engineering and Physics",linkedin:"https://www.linkedin.com/in/lucaszubillagamaharg/",email:"lucas@rise-reforming.com",backImage:{src:"/UChicago.jpg",alt:"Uchicago"}},{id:"3",name:"Jona van Oord",title:"Co-Founder / COO",photo:{src:"/Jona.png",alt:"Jona"},description:"Molecular Engineering",linkedin:"https://www.linkedin.com/in/jonavanoord/ ",email:"jona@rise-reforming.com",backImage:{src:"/UChicago.jpg",alt:"Uchicago"}},{id:"4",name:"Konstantina (Nina) Kritikos",title:"Founding Process Engineer",photo:{src:"/Nina.jpg",alt:"Nina"},description:"Chemical Engineering",linkedin:" https://www.linkedin.com/in/konstantina-kritikos-99a086186/",email:"nina@rise-reforming.com",backImage:{src:"/Northwestern.jpg",alt:"Northwestern"}}],h=[{id:"1",name:"Dr. Jack Lewnard",title:"Scientific Advisor",photo:{src:"/Jack.jpg",alt:"Jack"},description:"Former Program Director @ DOE ARPA-E, Former CTO @ Gas Technology Institute (GTI)"},{id:"2",name:"Dr. Mark Stoykovich",title:"Scientific Advisor",photo:{src:"/Mark.png",alt:"Mark"},description:"University of Chicago Molecular Engineering Senior Instructional Professor & Director of Undergraduate Studies"},{id:"3",name:"Richard Penning",title:"Business Advisor",photo:{src:"/Richard.jpg",alt:"Richard"},description:"Former General Manager, Catalysts, UOP LLC; advisor to Nanograf and Numat Technologies"},{id:"4",name:"Bill Frauenheim",title:"Industry Liaison",photo:{src:"/Bill.png",alt:"Bill"},description:"VP of Operations @ Diversified CPC (largest aerosol propellant manufacturer in North America)"}];export{h as A,t as C,d as E,c as L,s as M,l as R,g as b};
--- assets/TechOne-D4Jr4-tO.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/TechDark-MiKz7Qn4.png",a="/assets/TechTwo-BlyCRFWq.png",e="/assets/TechOne-DHlPFAiQ.png";export{a,s as b,e as c};
